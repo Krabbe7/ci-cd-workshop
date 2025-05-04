222abb8a2d1448e1948f40ff4f4fe23420e00797
Nichlas <nichlas.flindt.krabbe@gmail.com>
2025-05-04 15:30:53 +0200

    Fjernet frontend i yml så playwright selv starter serveren

--- web/playwright.config.js
+++ web/playwright.config.js
@@ -5,7 +5,7 @@ module.exports = {
     //Playwright vil vente op til 30.000 millisekunder (eller 30 sekunder) på, at serveren (der kører i webServer)
     // starter og bliver klar til at modtage forespørgsler.
     timeout: 30000,
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
   },
   // så jeg kan bruge "/" i stedet for 'http://localhost:3000' i test med playwright
   use: {
